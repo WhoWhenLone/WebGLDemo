85f9577f5c4bd78f0d36838c68527fa98c20e3bb
hu2878375578 <hu2878375578@users.noreply.github.com>
2018-04-26 16:43:25 +0800

    FboDemo
    利用帧缓冲实现二次绘制

---
+++ FBODemo/js/Triangle.js
@@ -0,0 +1,78 @@
+function Triangle(								//声明绘制用物体对象所属类
+	gl,						 					//GL上下文
+	programIn									//着色器程序id
+){
+	//this.vertexData=vertexDataIn;						//初始化顶点坐标数据
+	this.vertexData=
+		[
+            -3,2,0,
+            3,-2,0,
+            3,2,0,
+
+            -3,2,0,
+            -3,-2,0,
+            3,-2,0
+
+		];
+	this.vcount=this.vertexData.length/3;					//得到顶点数量
+	this.vertexBuffer=gl.createBuffer();				//创建顶点坐标数据缓冲
+	gl.bindBuffer(gl.ARRAY_BUFFER,this.vertexBuffer); 	//绑定顶点坐标数据缓冲
+	//将顶点坐标数据送入缓冲
+	gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(this.vertexData),gl.STATIC_DRAW);
+	
+	this.colorsData=
+		[
+			0,0,1,1,1,0,
+			0,0,0,1,1,1
+		];
+	this.colorBuffer=gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER,this.colorBuffer); 	//绑定颜色数据缓冲
+	//将颜色数据送入缓冲
+	gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(this.colorsData),gl.STATIC_DRAW);
+
+	
+	this.program=programIn;		//初始化着色器程序id
+	
+	this.drawSelf=function(ms,tex,tex1)//绘制物体的方法
+	{	
+		gl.useProgram(this.program);//指定使用某套着色器程序
+		//执行平移
+		ms.translate(0,0,0);
+        // //执行绕Y轴旋转
+		// ms.rotate(currentYAngle,0,1,0);
+
+		//获取总变换矩阵引用id
+		var uMVPMatrixHandle=gl.getUniformLocation(this.program, "uMVPMatrix");
+		//将总变换矩阵送入渲染管线
+		gl.uniformMatrix4fv(uMVPMatrixHandle,false,new Float32Array(ms.getFinalMatrix()));
+		
+		gl.enableVertexAttribArray(gl.getAttribLocation(this.program, "aPosition"));//启用顶点坐标数据数组
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);	//绑定顶点坐标数据缓冲
+		//给管线指定顶点坐标数据
+		gl.vertexAttribPointer(gl.getAttribLocation(this.program,"aPosition"),3,gl.FLOAT,false,0, 0);
+		
+			//启用顶点纹理坐标数据数组
+		gl.enableVertexAttribArray(gl.getAttribLocation(this.program, "aTexCoor")); 
+		//绑定顶点纹理坐标数据缓冲
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.colorBuffer);
+		//给管线指定顶点纹理坐标数据
+		gl.vertexAttribPointer(gl.getAttribLocation(this.program, "aTexCoor"), 2, gl.FLOAT, false, 0, 0); 
+
+		var isIma = gl.getUniformLocation(this.program,"isImage");
+		if(tex)
+		{
+			gl.activeTexture(gl.TEXTURE0);
+			gl.bindTexture(gl.TEXTURE_2D,tex1);
+			gl.uniform1i(isIma,true);
+		}else {
+			gl.uniform1i(isIma,false);
+		}
+
+		//设置纹理
+		gl.uniform1i(gl.getUniformLocation(this.program, "sTexture"), 0);//将纹理送入渲染管线  	
+		gl.drawArrays(gl.TRIANGLES, 0, this.vcount);		//用顶点法绘制物体
+		//取消绑定
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+        gl.bindTexture(gl.TEXTURE_2D, null);
+	}
+}
---
+++ FBODemo/js/util/GLUtil.js
@@ -0,0 +1,95 @@
+//加载单个着色器的方法
+function loadSingleShader(ctx, shaderScript)
+{
+    if (shaderScript.type == "vertex")//若为顶点着色器
+    {
+        var shaderType = ctx.VERTEX_SHADER;//顶点着色器类型
+    }
+    else if (shaderScript.type == "fragment")//若为片元着色器
+    {
+        var shaderType = ctx.FRAGMENT_SHADER;//片元着色器类型
+    }
+
+    else {//否则打印错误信息
+        console.log("*** Error: shader script of undefined type '"+shaderScript.type+"'");
+        return null;
+    }
+
+    //根据类型创建着色器程序
+    var shader = ctx.createShader(shaderType);
+
+    //加载着色器脚本
+    ctx.shaderSource(shader, shaderScript.text);
+
+    //编译着色器
+    ctx.compileShader(shader);
+
+    //检查编译状态
+    var compiled = ctx.getShaderParameter(shader, ctx.COMPILE_STATUS);
+    if (!compiled && !ctx.isContextLost()) {//若编译出错
+        var error = ctx.getShaderInfoLog(shader);//获取错误信息
+        console.log("*** Error compiling shader :"+error);//打印错误信息
+        ctx.deleteShader(shader);//删除着色器程序
+        return null;//返回空
+    }
+    return shader;//返回着色器程序
+}
+
+//加载链接顶点、片元着色器的方法
+function loadShaderSerial(gl, vshader, fshader)
+{
+    //加载顶点着色器
+    var vertexShader = loadSingleShader(gl, vshader);
+    //加载片元着色器
+    var fragmentShader = loadSingleShader(gl, fshader);
+
+    //创建着色器程序
+    var program = gl.createProgram();
+
+    //将顶点着色器和片元着色器挂接到着色器程序
+    gl.attachShader (program, vertexShader);//将顶点着色器添加到着色器程序中
+    gl.attachShader (program, fragmentShader);//将片元着色器添加到着色器程序中
+
+    //链接着色器程序
+    gl.linkProgram(program);
+
+    //检查链接是否成功
+    var linked = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (!linked && !gl.isContextLost())//若链接不成功
+    {
+        //获取并在控制台打印错误信息
+        var error = gl.getProgramInfoLog (program);//获取错误信息
+        console.log("Error in program linking:"+error);//打印错误信息
+
+        gl.deleteProgram(program);//删除着色器程序
+        gl.deleteProgram(fragmentShader);//删除片元着色器
+        gl.deleteProgram(vertexShader);//删除顶点着色器
+
+        return null;//返回空
+    }
+    gl.useProgram(program);//
+    gl.enable(gl.DEPTH_TEST);//打开深度检测
+    return program;//返回着色器程序
+}
+
+//加载纹理图的方法
+function loadImageTexture(gl,url,texName)
+{
+    var texture = gl.createTexture();
+    var image = new Image();
+    image.onload = function() { doLoadImageTexture(gl, image, texture) }
+    image.src = url;
+    //texMap.set(texName,texture);
+    texMap[texName]=texture;
+}
+
+function doLoadImageTexture(gl, image, texture)
+{
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+}
